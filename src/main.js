1088a5cc07980029daeb6ef8cd732c2e10b89244
consciousness_dev <consciousness.dev@gmail.com>
2021-04-10 12:17:49 +0700

    chore: setup buefy - import - reinstall sass-loader to 10.1.1 version

--- src/main.js
+++ src/main.js
@@ -3,6 +3,15 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 
+import Buefy from 'buefy';
+import './assets/styles/style.scss';
+import '@mdi/font/css/materialdesignicons.css';
+
+Vue.use(Buefy, {
+  defaultIconPack: 'mdi',
+  defaultContainerElement: '#content',
+});
+
 Vue.config.productionTip = false;
 
 new Vue({
