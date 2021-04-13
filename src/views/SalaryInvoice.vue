d7fe648fe99ee7f161cf52709dbb59898b3dc156
consciousness_dev <consciousness.dev@gmail.com>
2021-04-13 07:13:20 +0700

    styling: salary invoice page - top

---
+++ src/views/SalaryInvoice.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="salary__invoice">
+    <div class="salary__invoice-wrapper">
+      <header-apps
+        :title="'Faktur Gaji'"
+        :personName="'Bu Adara Olivia'"
+        :salaryPeriode="'01 Januari 2021 - 31 Januari 2021'"
+        :presenceDay="'22'"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HeaderApps from '@/components/HeaderApps';
+
+export default {
+  name: 'Home',
+  components: {
+    HeaderApps,
+  },
+};
+</script>
